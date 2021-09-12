64568146fc30c3258ef43d62405b0a217b450883
Anderson <BlackCode7@outlook.com>
2021-09-12 13:14:03 -0300

    Adicionando os componentes de navbar-footer-tabela

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return (
+     
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                
+                <p className="text-light">Java / Spring Boot / React / TypeScript <a href="https://github.com/BlackCode7" 
+                target="_blank" rel="noreferrer">GitHub</a></p>
+
+                <p className="text-light">Meu contato no Whatsapp <a href="https://api.whatsapp.com/send?phone=5521990431613&text=Ol%C3%A1%20seja%20bem-vindo%20ao%20meu%20perfil!" 
+                target="_blank" rel="noreferrer">Whatsapp</a></p>
+                
+                <p className="text-light"><small><strong></strong>Meu contato no Instagram <a href="https://www.instagram.com/andersonribeirom/" 
+                target="_blank" rel="noreferrer">Instagram</a></small></p>
+            </div>
+        </footer>
+      
+    );
+  }
+  
+  export default Footer;
